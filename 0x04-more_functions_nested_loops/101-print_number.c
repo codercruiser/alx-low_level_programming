7e3c4a62c986d54e5adb1fc7883cbc94187e681d
sam <99776941+codercruiser@users.noreply.github.com>
2023-02-23 14:30:08 +0100

    Update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,82 +1,27 @@
 #include "main.h"
 
-
-int _pow(int a, int b);
-
 /**
- * print_number - print the given number.
- * @n: number to print.
+ * print_number - prints an integer
+ * @n: integer to be printed
  */
 void print_number(int n)
 {
-	int collection = n;
-	unsigned int new_num;
-	int count = 0;
-	int print = 0;
+unsigned int n0;
 
-	if (n < 0)
-	{
-		new_num = -n;
-		_putchar('-');
-	}
-	else
-	{
-		new_num = n;
-	}
-
-	while (collection != 0)
-	{
-		collection /= 10;
-		++count;
-	}
-	count -= 1;
-
-	do {
-		if ((new_num / (_pow(10, count)) > 9) && count > 1)
-		{
-			print = new_num / (_pow(10, count));
-			print = print % 10;
-			_putchar('0' + print);
-		}
-		else if (count > 1)
-		{
-			print = (new_num / _pow(10, count));
-		       _putchar('0' + print);
-		}
-		else if (((new_num / 10) > 9) && count == 1)
-		{
-			print = (new_num / 10) % 10;
-			_putchar('0' + print);
-		}
-		else if (count == 1)
-		{
-			print = new_num / 10;
-			_putchar('0' + print);
-		}
-		else
-		{
-			print = new_num % 10;
-			_putchar('0' + print);
-		}
-		count--;
-	} while (count > -1);
+if (n < 0)
+{
+n0 = -n;
+_putchar('-');
+}
+else
+{
+n0 = n;
 }
 
-/**
- * _pow - power fuction
- * @a: value to use.
- * @b: growth count
- *
- * Return: value int.
- */
-int _pow(int a, int b)
+if (n0 / 10)
 {
-	int i;
-	int p = a;
+print_number(n0 / 10);
+}
 
-	for (i = 1 ; i < b ; i++)
-	{
-		a *= p;
-	}
-	return (a);
+_putchar((n0 % 10) + '0');
 }
