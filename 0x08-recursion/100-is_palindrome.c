8344db57e01b9b145b6fb8971e85f761626956a2
sam <99776941+codercruiser@users.noreply.github.com>
2023-03-08 18:32:01 +0100

    Update 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,17 +1,52 @@
-#include "holberton.h"
+#include "main.h"
+
 /**
- * is_palindrome - find a palindrome string
- * @s: string for check
- *
- * Return: 1 if is palindrome, otherwise 0
+ * lenght - Find the length of a srting.
+ * @s: String.
+ * Return: The lenght of the string.1
  */
+int length(char *s)
+{
+	if (*s)
+	{
+		return (1 + length(s + 1));
+	}
+
+	return (0);
+}
+
+/**
+  * function - Have the condition to define if it is a palindrome.
+  * @s: String.
+  * @len: Lenght of the string.
+  * @count: Counter of the function.
+  * Return: 1 if it is a palindrome and 0 if it doesn't.
+  */
+int function(char *s, int act_len, int count)
+{
+	if (count > act_len)
+	{
+		return (1);
+	}
+	else if (*(s + count) == *(s + act_len))
+	{
+		return (function(s, act_len - 1, count + 1));
+	}
+	else
+	{
+		return (0);
+	}
+}
+
+/**
+  * is_palindrome - Answer if the string is a palindrome.
+  * @s: String.
+  * Return: 1 if it is palindrome and 0 if it doesn't.
+  */
 int is_palindrome(char *s)
 {
-	int i;
+	int act_len = lenght(s) - 1;
+	int count = 0;
 
-	i = 0;
-
-	if (*s == i)
-		return (0);
-	return (1);
+	return (function(s, act_len, count));
 }
