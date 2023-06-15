1adf83fa9783caad5645e36ad19f1712a114f910
sam <99776941+codercruiser@users.noreply.github.com>
2023-06-15 17:32:59 +0100

    Update 2-add_dnodeint.c

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,36 +1,34 @@
 #include "lists.h"
 
 /**
- * add_dnodeint - Adds a new node at the beginning of a doubly linked list
- * @head: Pointer to a pointer to the head of the linked list
- * @n: Data for each node
+ * add_dnodeint - function with two arguments
+ * @head: pointer to double linked list
+ * @n: data for each node
  *
- * Return: Address of the new element or NULL if failed
+ * Description: adds a new node at the beginning
+ * Return: address of new element or NULL if failed
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-    dlistint_t *new_node = NULL;
+	dlistint_t *new = NULL;
 
-    new_node = malloc(sizeof(dlistint_t));
-    if (!new_node)
-        return (NULL);
+	new = malloc(sizeof(dlistint_t));
+	if (!new)
+		return (NULL);
+	new->n = n;
+	new->prev = NULL;
 
-    new_node->n = n;
-    new_node->prev = NULL;
-
-    if (*head == NULL)
-    {
-        new_node->next = NULL;
-        *head = new_node;
-    }
-    else
-    {
-        new_node->next = *head;
-        if (*head)
-            (*head)->prev = new_node;
-        *head = new_node;
-    }
-
-    return new_node;
+	if (*head == NULL)
+	{
+		new->next = NULL;
+		*head = new;
+	}
+	else
+	{
+		new->next = *head;
+		if (*head)
+			(*head)->prev = new;
+		*head = new;
+	}
+	return (new);
 }
-
